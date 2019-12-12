be863a06d6092741704681f84fd54e12d7d851ca
Daniele <daniele_moschini@hotmail.it>
2019-12-12 09:58:19 +0100

    [UPDATE] Ordine Classi
    Ordine al codice

---
+++ justmeetfrontend/lib/classi/evento.dart
@@ -0,0 +1,12 @@
+ class Evento {
+      final String id;
+      final String titolo;
+      final String desc;
+    
+      Evento(this.id, this.titolo, this.desc);
+    
+      Evento.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+      titolo = json['titolo'],
+      desc = json['desc'];
+}
