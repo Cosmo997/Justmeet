cefd61c635ca92722d750d76328c7523d9e4b97b
Michele-x98 <45004856+Michele-x98@users.noreply.github.com>
2020-02-05 19:43:35 +0100

    [UPDATE] Aggiunto Drawer

--- justmeetfrontend/lib/view/routes/homepageanonima.dart
+++ justmeetfrontend/lib/view/routes/homepageanonima.dart
@@ -17,11 +17,37 @@ class HomepageAnonimaState extends State<HomepageAnonima>{
   Widget build(BuildContext context) {
   return Scaffold(
     appBar:AppBar(
+
                backgroundColor: ThemeHandler.jmTheme().primaryColor,
                elevation: 10,
                title: Image.asset('assets/images/logo.png', scale: 2.5),
                centerTitle: true,
                ),
+               drawer: Drawer(
+                   elevation: 20,
+                   child: Column(
+                     children: <Widget>[
+                       ListTile(
+                         title: Text("Crea Evento",style: TextStyle(fontSize: 20)),
+                         leading: Icon(Icons.add_circle, color: ThemeHandler.jmTheme().accentColor,),
+                         onTap: () {
+                         },
+                       ),
+                       ListTile(
+                         title: Text("Preferiti",style: TextStyle(fontSize: 20)),
+                         leading: Icon(Icons.favorite, color: ThemeHandler.jmTheme().accentColor,),
+                         onTap: () {
+                         },
+                       ),
+                       ListTile(
+                         title: Text("Area Riservata",style: TextStyle(fontSize: 20)),
+                         leading: Icon(Icons.accessible_forward, color: ThemeHandler.jmTheme().accentColor,),
+                         onTap: () {
+                         },
+                       ),
+                     ],
+                   ),
+                 ),
     body: FutureBuilder(
       future: ControllerJM.loadEvents(),
       builder: (BuildContext context, AsyncSnapshot<List<Evento>> snapshot) {
@@ -40,107 +66,65 @@ class HomepageAnonimaState extends State<HomepageAnonima>{
               Evento evento = snapshot.data[index];
               return  SingleChildScrollView(
                         child: Container(
-                        padding: EdgeInsets.all(20),
-                        margin: EdgeInsets.all(30),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                          color: ThemeHandler.jmTheme().secondaryHeaderColor,
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          ),
-                          child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                
-                              Card(
-                                elevation: 10,
-                                
-                                child: Container(
-                                  margin: EdgeInsets.all(15),
-                                  child: Column(
-                                    
-                                    children: <Widget>[
-                                      ListTile(
-                                  title: Text(evento.titolo),
-                                  subtitle: Text(evento.descrizione),
-                                  trailing: Icon(Icons.favorite_border), onTap: () => {}
-                                  //Se loggato, salva l'evento tra i preferiti.
-                                ),
-                                      Column(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: <Widget>[
-
-                                          //Inizio Evento
-                                          Container(
-                                            alignment: Alignment.center,
-                                            padding: EdgeInsets.all(2),
-                                            child:Text(
-                                              "Inizio Evento: " + _df.format(evento.inizioEvento),
-                                              style: TextStyle(color: ThemeHandler.jmTheme().primaryColor)),
-                                            decoration:BoxDecoration(
-                                              border: Border.all(color: ThemeHandler.jmTheme().primaryColor),
-                                              borderRadius: BorderRadius.all(Radius.circular(20)),
-                                              color: ThemeHandler.jmTheme().accentColor),
-                                            ),
-                                          //Fine Evento
-                                          Container(
-                                            alignment: Alignment.center,
-                                            padding: EdgeInsets.all(2),
-                                            decoration:BoxDecoration(
-                                              border: Border.all(color: ThemeHandler.jmTheme().primaryColor),
-                                              borderRadius: BorderRadius.all(Radius.circular(20)),
-                                              color: ThemeHandler.jmTheme().accentColor),
-                                            child: Text(
-                                              "Fine Evento: " + _df.format(evento.fineEvento),
-                                              style: TextStyle(color: ThemeHandler.jmTheme().primaryColor),
-                                              )),
-                                          //Posti Disponibili
-                                          Container(
-                                            alignment: Alignment.center,
-                                            padding: EdgeInsets.all(2),
-                                            child:Text(
-                                              "Posti disponibili: "+ evento.partecipanti.toString(),
-                                              style: TextStyle(color: ThemeHandler.jmTheme().primaryColor),
-                                              ),
-                                            decoration:BoxDecoration(
-                                              border: Border.all(color: ThemeHandler.jmTheme().primaryColor),
-                                              borderRadius: BorderRadius.all(Radius.circular(20)),
-                                              color: ThemeHandler.jmTheme().accentColor)
-                                              ),
-                                          //Topic
-                                          Container(
-                                            alignment: Alignment.center,
-                                            padding: EdgeInsets.all(2),
-                                            decoration:BoxDecoration(
-                                              border: Border.all(color: ThemeHandler.jmTheme().primaryColor),
-                                              borderRadius: BorderRadius.all(Radius.circular(20)),
-                                              color: ThemeHandler.jmTheme().accentColor), 
-                                            child: Text(
-                                              "Topic: "+ evento.idTopic.toString(),
-                                              style: TextStyle(color: ThemeHandler.jmTheme().primaryColor),
-                                                )
-                                              ),
-                                        ],
-                                        ),
-                                      
-                              //          ListTile(
-                              //           title:Text(_df.format(evento.inizioEvento)),
-                              //           subtitle: Text(_df.format(evento.fineEvento)),
-                              //           trailing: Text("Numero partecipanti:"+evento.partecipanti.toString() +"\n\nTopic: "+ evento.idTopic.toString()),
-                              // ),
-                                  ],),
-                                ),
-                              ),
-                              RaisedButton(
-                                elevation: 2,
-                                shape: RoundedRectangleBorder(
-                                borderRadius: new BorderRadius.circular(8.0),
-                                ),
-                                child: Text("ISCRIVITI"),
-                                color: ThemeHandler.jmTheme().accentColor,
-                                onPressed: ()=>{}
-                                //,
-                              ),
-                            ],
+                          child: Center(
+                            child: Card(
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                              color: ThemeHandler.jmTheme().secondaryHeaderColor,
+                              elevation: 10,
+                              margin: EdgeInsets.all(20),
+                              child: Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    leading: CircleAvatar(
+                                      child:Image.asset('assets/images/iconaUser.png'),
+                                    ),
+                                    title: Text(evento.titolo),
+                                    subtitle: Text(evento.idCreatore),
+                                  ),
+                                   Divider(indent: 30, endIndent: 30, height: 10, thickness: 2,),
+                                   Text("Descrizione", style: TextStyle(fontSize: 18), ),
+                                   Container(
+                                     height: 150,
+                                     width: 1000,  
+                                     child: Card(
+                                       color: Colors.grey,
+                                       margin: EdgeInsets.fromLTRB(15, 5, 15, 10),
+                                       child: SingleChildScrollView(
+                                         child: Padding(
+                                           padding: const EdgeInsets.all(10.0),
+                                           child: Text(evento.descrizione),
+                                         )),
+                                     ),
+                                   ),
+                                   Divider(indent: 30, endIndent: 30, height: 10, thickness: 2,),
+                                  Container(
+                                    margin: EdgeInsets.all(20),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: <Widget>[
+                                      Icon(Icons.people, color: ThemeHandler.jmTheme().accentColor,),
+                                      Text(" Partecipanti: "),
+                                      Text(evento.partecipanti.toString()),
+                                      Icon(Icons.category,  color: ThemeHandler.jmTheme().accentColor),
+                                      Text(" Topic: "),
+                                      Text(evento.idTopic),
+                                    ],),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.all(20),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: <Widget>[
+                                      Icon(Icons.calendar_today,  color: ThemeHandler.jmTheme().accentColor),
+                                      Text(_df.format(evento.inizioEvento)),
+                                      Icon(Icons.calendar_today,  color: ThemeHandler.jmTheme().accentColor),
+                                      Text(_df.format(evento.inizioEvento)),
+                                    ],),
+                                  ),
+                                    Icon(Icons.place, color: ThemeHandler.jmTheme().accentColor),
+                                    Text("Comune: " +evento.idComune),
+                                ],
+                              )
                             ),
                           ),
                         ),
