4cd7facf7e35df03367761b735c5030e0cfec204
Michele-x98 <45004856+Michele-x98@users.noreply.github.com>
2020-02-14 17:19:20 +0100

    [UPDATE] Aggiunto Test ControllerEvento

--- justmeetbackend/src/test/java/it/unicam/justmeetbackend/DemoApplicationTests.java
+++ justmeetbackend/src/test/java/it/unicam/justmeetbackend/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package it.unicam.justmeetbackend;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import it.unicam.justmeetbackend.controllers.EventoController;
+
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
+
+	@Autowired
+	private EventoController controller;
 
 	@Test
-	void contextLoads() {
+	public void contexLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
-
 }
