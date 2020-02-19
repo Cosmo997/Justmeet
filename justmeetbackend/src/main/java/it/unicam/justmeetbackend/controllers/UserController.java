4ecd5cc8dd8d10ec31606b8147ce600887f294f0
Michele-x98 <45004856+Michele-x98@users.noreply.github.com>
2020-02-19 17:11:10 +0100

    [UPDATE]

--- justmeetbackend/src/main/java/it/unicam/justmeetbackend/controllers/UserController.java
+++ justmeetbackend/src/main/java/it/unicam/justmeetbackend/controllers/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-public class UserController {
+public class UserController{
 
     @Autowired
     private UserRepository repository;
--- justmeetbackend/src/test/java/it/unicam/justmeetbackend/DemoApplicationTests.java
+++ justmeetbackend/src/test/java/it/unicam/justmeetbackend/DemoApplicationTests.java
@@ -8,15 +8,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import it.unicam.justmeetbackend.controllers.EventoController;
+import it.unicam.justmeetbackend.controllers.TopicController;
+import it.unicam.justmeetbackend.controllers.UserController;
 
 @SpringBootTest
-public class DemoApplicationTests {
+public class DemoApplicationTests{
+	
+	@Autowired
+	UserController user;
 
 	@Autowired
-	private EventoController controller;
+	EventoController evento;
+
+	@Autowired
+	TopicController topic;
 
 	@Test
-	public void contexLoads() throws Exception {
-		assertThat(controller).isNotNull();
+	public void userControllerNotNull() throws Exception {
+		assertThat(user).isNotNull();
 	}
+
+	@Test
+	public void eventoControllerNotNUll() throws Exception {
+		assertThat(evento).isNotNull();
+	}
+
+	@Test
+	public void topicControllerNotNull() throws Exception {
+		assertThat(topic).isNotNull();
+	}
+
 }
